7f1e5ee2ef296de5073927bfe00ae894b2d110eb
xzl2021 <xzl2021@hotmail.com>
2020-03-09 18:41:54 +0800

    Add udp port map in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /etc/shadowsocks-libev/
-docker run -d -p 8443:443 -v /etc/shadowsocks-libev:/etc/shadowsocks-libev --name ss-libev xzl2021/shadowsocks_libev-obfs-nginx:latest
+docker run -d -p 8443:443 -p 8443:443/udp -v /etc/shadowsocks-libev:/etc/shadowsocks-libev --restart=always --name ss-libev xzl2021/shadowsocks_libev-obfs-nginx:latest
