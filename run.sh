385705d248a5e84b9f7cd46fa427222a94ecaf85
Lyraa <xzl2021@hotmail.com>
2022-10-08 18:19:26 +0800

    Use 0.0.0.0 to only bind ipv4 ports.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p /etc/shadowsocks-libev-obfs/
-docker run -d -p 8443:443 -p 8443:443/udp -v /etc/shadowsocks-libev-obfs:/etc/shadowsocks-libev --restart=always --name ss-obfs sslibev-obfs-nginx:latest
+docker run -d -p 0.0.0.0:8443:443 -p 0.0.0.0:8443:443/udp -v /etc/shadowsocks-libev-obfs:/etc/shadowsocks-libev --restart=always --name ss-obfs sslibev-obfs-nginx:latest
